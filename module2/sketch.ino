0871741dcc91fd43e84c54c1a9bf57b2345f7032
SDV7 <sdv.seven@yandex.ru>
2026-05-30 17:19:37 +0300

    Update sketch.ino

--- module2/sketch.ino
+++ module2/sketch.ino
@@ -1,4 +1,5 @@
 const int PIN_R = 8, PIN_Y = 9, PIN_G = 10;
+const int BTN1 = 2, BTN2 = 3;
 
 void setTL(char c) {
   digitalWrite(PIN_R, c == 'R');
@@ -11,6 +12,9 @@ void setup() {
   pinMode(PIN_R, OUTPUT);
   pinMode(PIN_Y, OUTPUT);
   pinMode(PIN_G, OUTPUT);
+  pinMode(BTN1, INPUT_PULLUP);
+  pinMode(BTN2, INPUT_PULLUP);
+  setTL('Y');
 }
 
 void loop() {
@@ -18,4 +22,6 @@ void loop() {
     char c = Serial.read();
     if (c == 'R' || c == 'Y' || c == 'G') setTL(c);
   }
+  if (!digitalRead(BTN1)) { Serial.println("B1"); delay(300); }
+  if (!digitalRead(BTN2)) { Serial.println("B2"); delay(300); }
 }
